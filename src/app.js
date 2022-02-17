70b83d8e357819cf522d3486d3fde0c5e2bbc4b9
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-02-17 17:25:09 +0100

    started work on this

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import RoomModel from './room/model.js'
 
 const app = express()
 
@@ -10,4 +11,10 @@ const app = express()
  app.get('/online-users', (req, res, next) =>{
      res.send({onlineUsers: onlineUsers })
  })
+
+ app.get('/chats/:room', async (req, res) => {
+    const room = await RoomModel.findOne({ name: req.params.room })
+
+    res.send({ messages: room.messages })
+})
  export default app
--- src/index.js
+++ src/index.js
@@ -1,20 +1,49 @@
-import express from 'express'
-import cors from 'cors'
 import { Server } from "socket.io";
 import { createServer } from 'http';
 import mongoose from 'mongoose'
 import app from './app.js'
+import RoomModel from "./room/model.js";
 
 
 const httpServer = createServer(app)
 const io = new Server(httpServer, { /* options */ });
-io.on("connection", (socket) => { 
-console.log(socket.id)
 
+io.on("connection", (socket) => {
+    console.log(socket.id, socket.rooms)
+
+    socket.on("setUsername", ({ username, room }) => {
+        console.log(username)
+
+       
+
+        onlineUsers.push({ username, id: socket.id, room })
+
+        socket.emit('loggedin')
+        socket.broadcast.emit('newConnection')
+    })
+
+    socket.on("sendmessage", async ({ message, room }) => {
+
+        try {
+            await RoomModel.findOneAndUpdate({ name: room }, {
+                $push: { messages: message }
+            })
+
+            socket.to(room).emit("message", message)
+
+        } catch (error) {
+            socket.emit("error", { error: "Can't save to DB. Try again later." })
+        }
+
+    })
+
+    socket.on("disconnect", () => {
+        onlineUsers = onlineUsers.filter(u => u.id !== socket.id)
+    })
 });
 
-mongoose.connect('mongodb+srv://Asadbek:Azamjonov@cluster0.ykjem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_CONNECTION)
 .then(() => {
     console.log("connected to mongo")
-    httpServer.listen(3030);
+    httpServer.listen(4000);
 })
